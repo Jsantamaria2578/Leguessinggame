a7a528d51a36baec24b156d60d672a5c783f1c87
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-09-25 12:56:26 -0400

    start

---
+++ guessinggame/src/guessinggame/GuessingGame.java
@@ -0,0 +1,27 @@
+package guessinggame;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class GuessingGame {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Guess a number between 1 and 10");
+			int guess = input.nextInt();
+			int answer = (int)(Math.random() * 10 + 1);
+			while(guess != answer) {
+				boolean isnumber = false;
+				while(!isnumber)
+				{
+					try {
+						if(guess > 5 || guess <= 5 ) 
+							isnumber = true;}
+					
+						catch(InputMismatchException e) {
+							System.out.println("Thats no number cheater! Try again.");
+							main(args);
+						}
+					}
+				}
+		}
+	}
+
+
