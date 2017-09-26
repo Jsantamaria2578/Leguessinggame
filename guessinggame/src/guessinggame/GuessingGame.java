52239fd6e3bb0d82fceaecef1ea8c249406f4780
BT_1N3_12 <BT_1N3_12@BT_1N3_12.bktech.local>
2017-09-26 13:14:58 -0400

    FINISHED YEET

--- guessinggame/src/guessinggame/GuessingGame.java
+++ guessinggame/src/guessinggame/GuessingGame.java
@@ -6,26 +6,32 @@ public class GuessingGame {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Guess a number between 1 and 10");
-			int guess = input.nextInt();
-			int answer = (int)(Math.random() * 10 + 1);
-			while(guess != answer) {
-				boolean isnumber = false;
-				while(!isnumber)
-				{
-					try {
-						if(guess > 5 || guess <= 5 ) 
-							isnumber = true;
-							System.out.println("Guess again");;
-							 guess = input.nextInt();}
-					
-						catch(InputMismatchException e) {
-							System.out.println("Thats no number cheater! Try again.");
-							main(args);
-						}
+			int userguess = input.nextInt();
+			int answer1 = (int)(Math.random() * 10 + 1);
+			checkWin(userguess,answer1);
+			System.out.println("you win!");
+	}
+	public static void checkWin(int guess, int answer){
+		Scanner input = new Scanner(System.in);
+		while(guess != answer) {
+			boolean isnumber = false;
+			while(!isnumber)
+			{
+				try {
+					if(guess > 5 || guess <= 5 ) {
+						isnumber = true;
+						System.out.println("Guess again");
+						 guess = input.nextInt();
+						 }
+				}
+				
+					catch(InputMismatchException e) {
+						System.out.println("Thats no number cheater! Try again.");
+						checkWin(guess,answer);
 					}
 				}
-			System.out.println("you win!");
 		}
-	}
-
+	
+	}}
+	
 
